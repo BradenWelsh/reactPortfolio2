e2037113fc8ee182753aa8a3b300993d0f398140
Braden Welsh <BradenWelsh@users.noreply.github.com>
2021-07-19 18:58:39 -0500

    made it to where the projects take you to the project repo

--- src/comps/pages/Projects/AllProjects.js
+++ src/comps/pages/Projects/AllProjects.js
@@ -1,18 +1,17 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 
 function AllProjects(props){
     return (
         <>
         <li className="projectItems">
-        <Link className='projectItem-Link' to={props.path} href={props.link} target='_blank'>
+        <a className='projectItem-Link' href={props.link} target='_blank' rel='noreferrer'>
             <figure className='projectImgWrap' data-category={props.label}>
                 <img src={props.src} alt='Project Image' className='projectImg'/>
             </figure>
             <div className='projectInfo'>
                 <h5 className='projectText'>{props.text}</h5>
             </div>
-        </Link>
+        </a>
         </li>
         </>
     )
--- src/comps/pages/Projects/Project.js
+++ src/comps/pages/Projects/Project.js
@@ -9,13 +9,13 @@ function Project () {
             <div className='projectContainer'>
                 <div className='projectWrap'>
                     <ul className='projectItem'>
-                        <AllProjects  src='images/runbuddy.png' label= 'Run-Buddy' text='HTML-CSS' to='' path='/Projects' />
+                        <AllProjects  src='images/runbuddy.png' label= 'Run-Buddy' text='HTML-CSS' link='https://github.com/BradenWelsh/run-buddy' path='/Projects' />
 
-                        <AllProjects  src='images/site.png' label= 'Horiseon' text='HTML-CSS' link='' path='/Projects' />
+                        <AllProjects  src='images/site.png' label= 'Horiseon' text='HTML-CSS' link='https://github.com/BradenWelsh/weekly-challenge-01' path='/Projects' />
                         
                         <AllProjects  src='images/gif.gif' label= 'Employee Tracker' text='mySQL, inquirer, and figlet.' link='https://github.com/BradenWelsh/employeeTracker/' path='/Projects' />
 
-                        <AllProjects  src='images/ss2.png' label= 'Fantasy Team Builder' text='Bcrypt, node, express, handle bars, mySQL, dotenv, sequelize' link= '' path='/Projects' />
+                        <AllProjects  src='images/ss2.png' label= 'Fantasy Team Builder' text='Bcrypt, node, express, handle bars, mySQL, dotenv, sequelize' link= 'https://github.com/BradenWelsh/fantasy-team-roster' path='/Projects' />
                     </ul>
                 </div>
             </div>
